26b2688da905e5760d0b300e9a8004f0aa008cde
Phuoc Hiep <zendragon93@gmail.com>
2020-07-08 11:46:52 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef } from "react";
+import "./styles.css";
+import videojs from "video.js";
+import vjsqs from "@silvermine/videojs-quality-selector";
+import "@silvermine/videojs-quality-selector/dist/css/quality-selector.css";
+
+export default function App() {
+  const playerRef = useRef();
+  const videoJsOptions = {
+    // autoplay: false,
+    // playbackRates: [0.5, 1, 1.25, 1.5, 2],
+    // width: 720,
+    // height: 300,
+    controls: true,
+    // fill: true,
+    controlBar: {
+      children: [
+        "playToggle",
+        "progressControl",
+        "volumePanel",
+        "qualitySelector",
+        "fullscreenToggle"
+      ]
+    },
+    sources: [
+      {
+        src:
+          "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+        type: "application/x-mpegURL",
+        label: "HDd",
+        res: 720
+      },
+      {
+        src: "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8",
+        type: "application/x-mpegURL",
+        label: "HD2",
+        res: 720
+      }
+    ]
+  };
+  useEffect(() => {
+    vjsqs(videojs);
+    const player = videojs(playerRef.current, videoJsOptions, () => {
+      console.log(player.controlBar);
+      player.controlBar.addChild("QualitySelector");
+    });
+    return () => {
+      player.dispose();
+    };
+  }, []);
+  return (
+    <>
+      <div data-vjs-player>
+        <video
+          ref={playerRef}
+          className="video-js vjs-4-3"
+          data-setup='{"liveui": false}'
+        />
+      </div>
+    </>
+  );
+}
